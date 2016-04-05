2afbdf4b2f2c98b59174b3de871dada216fb47fb
Andre Bonfatti <andre.bonfatti@movile.com>
2016-04-04 23:31:46 -0300

    progressing w project

--- comp6/Compiler.java
+++ comp6/Compiler.java
@@ -1,31 +1,31 @@
 /*
     comp6
-    
-    
-    We added to the grammar more operators and a declaration of variables so that 
-    the grammar now accepts a program like 
+
+
+    We added to the grammar more operators and a declaration of variables so that
+    the grammar now accepts a program like
           a = 1  b = 3  : (- (+ a 2) 3)
     The result of the evaluation would be 0. Another program would be
           g = 3  t = 9  : (* (- t 7) g)
 
     Of course, the AST was extended to cope with the new rules. New classes
-    were created: 
+    were created:
          Program - represents the program
          Variable - a variable in the declaration
          VariableExpr - a variable inside an expression
-         
+
     Grammar:
-       Program ::= VarDecList ':' Expr   
-       VarDecList ::=  | VarDec VarDecList 
+       Program ::= VarDecList ':' Expr
+       VarDecList ::=  | VarDec VarDecList
        VarDec ::= Letter '=' Number
        Expr::=  '(' oper Expr  Expr ')' | Number | Letter
        Oper ::= '+' | '-' | '*' | '/'
        Number ::= '0'| '1' | ... | '9'
        Letter ::= 'A' | 'B'| ... | 'Z'| 'a'| 'b' | ... | 'z'
-       
-     
+
+
 */
- 
+
 import AST.*;
 import java.util.ArrayList;
 
@@ -35,16 +35,16 @@ public class Compiler {
         input = p_input;
         tokenPos = 0;
         nextToken();
-        
-        
+
+
         Program e = program();
         if (tokenPos != input.length)
           error();
-          
+
         return e;
     }
 
-	//Program ::= VarDecList ':' Expr   
+	//Program ::= VarDecList ':' Expr
     private Program program(){
 		ArrayList<Variable> esq = varDecList();
 		Expr dir = null;
@@ -53,12 +53,12 @@ public class Compiler {
 			dir = expr();
 		}else
 			error();
-	
+
 		return new Program(esq,dir);
     }
     
-    
-    //VarDecList ::=  | VarDec VarDecList 
+
+    //VarDecList ::=  | VarDec VarDecList
     private ArrayList<Variable> varDecList(){
 		ArrayList<Variable> retorno = new ArrayList<Variable>();
 		while(token != ':'){
@@ -67,8 +67,8 @@ public class Compiler {
 
 		return retorno;
     }
-    
-    
+
+
     // VarDec ::= Letter '=' Number
 	private Variable varDec(){
 		char aux = letter();
@@ -83,44 +83,44 @@ public class Compiler {
 
     //Expr::=  '(' oper Expr  Expr ')' | number | Letter
     private Expr expr() {
-		
-		Expr res = null;		
+
+		Expr res = null;
 
 		if(token == '('){
 			nextToken();
 			char op;
-			op = oper();						
+			op = oper();
 			Expr ExprE, ExprD;
 			ExprE = expr();
 			ExprD = expr();
 
 			if(token == ')'){
 				nextToken();
-			
+
 				res = new CompositeExpr(op, ExprE, ExprD);
 
 			}
 			else
-				error();						
+				error();
 		}
-		else 
+		else
 			if(token >= '0' && token <= '9')
-				res = number();		
+				res = number();
 			else
 				res = new VariableExpr(letter());
 
-		return res;	
+		return res;
     }
-    
-    
+
+
     //oper ::= '+' | '-'
     private char oper() {
-		char token1 = ' ';	
+		char token1 = ' ';
 		if(token == '+' || token == '-'){
-			token1 = token;			
+			token1 = token;
 			nextToken();
 		}
-		else 
+		else
 			error();
 
 		return token1;
@@ -128,18 +128,18 @@ public class Compiler {
 
     //number ::= '0'| '1' | ... | '9'
     private NumberExpr number() {
-		NumberExpr number = null;     
-		
+		NumberExpr number = null;
+
 		if(token >= '0' && token <= '9'){
-			number = new NumberExpr(token);			
-			nextToken();		
+			number = new NumberExpr(token);
+			nextToken();
 		}
 		else
-			error();	
-		
+			error();
+
 		return number;
 	}
-    
+
     private char letter(){
 		char aux = ' ';
 		if(token >= 'a' && token <= 'z'){
@@ -164,22 +164,22 @@ public class Compiler {
 		}
 
     }
-    
+
     private void error() {
-        if ( tokenPos == 0 ) 
-          tokenPos = 1; 
-        else 
+        if ( tokenPos == 0 )
+          tokenPos = 1;
+        else
           if ( tokenPos >= input.length )
             tokenPos = input.length;
-        
+
         String strInput = new String( input, tokenPos - 1, input.length - tokenPos + 1 );
         String strError = "Error at \"" + strInput + "\"";
         System.out.println( strError );
         throw new RuntimeException(strError);
     }
-    
+
     private char token;
     private int  tokenPos;
     private char []input;
-      
+
 }
--- project/1/Compiler.java
+++ project/1/Compiler.java
@@ -8,34 +8,34 @@ Projeto de Implementação - Fase 1
 
 == Grammar
 
-    Program      ::= Decl
-    Decl         ::= 'v' 'm' '(' ')' StmtBlock
-    StmtBlock    ::= '{' { VariableDecl } { Stmt } '}'
-    VariableDecl ::= Variable ';'
-    Variable     ::= Type Ident
-    Type         ::= StdType | ArrayType
-    StdType      ::= 'i' | 'd' | 'c'
-    ArrayType    ::= StdType '[' ']'
-    Stmt         ::= Expr ';' | IfStmt | WhileStmt | BreakStmt | PrintStmt
-    IfStmt       ::= 'f' '(' Expr ')' '{' { Stmt } '}' [ 'e' '{' { Stmt } '}' ]
-    WhileStmt    ::= 'w' '(' Expr ')' '{' { Stmt } '}'
-    BreakStmt    ::= 'b' ';'
-    PrintStmt    ::= 'p' '(' Expr { ',' Expr }')'
-    Expr         ::= SimExpr [ RelOp Expr]
-    SimExpr      ::= [Unary] Term { AddOp Term }
-    Term         ::= Factor { MulOp Factor }
-    Factor       ::= LValue '=' Expr
-                        | LValue
-                        | '(' Expr ')'
-                        | 'r' '(' ')' | 's' '(' ')' | 't' '(' ')'
-    LValue       ::= Ident | Ident '[' Expr ']'
-    Ident        ::= Letter { Letter | Digit }
-    RelOp        ::= '=' | '#' | '<' | '>'
-    AddOp        ::= '+' | '-'
-    MulOp        ::= '*' | '/' | '%'
-    Unary        ::= '+' | '-' | '!'
-    Digit        ::= '0' | '1' | ... | '9'
-    Letter       ::= 'A' | 'B' | ... | 'Z' | 'a' | 'b' | ... | 'z'
+Program      ::= Decl
+Decl         ::= 'v' 'm' '(' ')' StmtBlock
+StmtBlock    ::= '{' { VariableDecl } { Stmt } '}'
+VariableDecl ::= Variable ';'
+Variable     ::= Type Ident
+Type         ::= StdType | ArrayType
+StdType      ::= 'i' | 'd' | 'c'
+ArrayType    ::= StdType '[' ']'
+Stmt         ::= Expr ';' | IfStmt | WhileStmt | BreakStmt | PrintStmt
+IfStmt       ::= 'f' '(' Expr ')' '{' { Stmt } '}' [ 'e' '{' { Stmt } '}' ]
+WhileStmt    ::= 'w' '(' Expr ')' '{' { Stmt } '}'
+BreakStmt    ::= 'b' ';'
+PrintStmt    ::= 'p' '(' Expr { ',' Expr }')'
+Expr         ::= SimExpr [ RelOp Expr]
+SimExpr      ::= [Unary] Term { AddOp Term }
+Term         ::= Factor { MulOp Factor }
+Factor       ::= LValue ':' Expr
+| LValue
+| '(' Expr ')'
+| 'r' '(' ')' | 's' '(' ')' | 't' '(' ')'
+LValue       ::= Ident | Ident '[' Expr ']'
+Ident        ::= Letter { Letter | Digit }
+RelOp        ::= '=' | '#' | '<' | '>'
+AddOp        ::= '+' | '-'
+MulOp        ::= '*' | '/' | '%'
+Unary        ::= '+' | '-' | '!'
+Digit        ::= '0' | '1' | ... | '9'
+Letter       ::= 'A' | 'B' | ... | 'Z' | 'a' | 'b' | ... | 'z'
 
 */
 
@@ -44,163 +44,276 @@ import java.util.ArrayList;
 
 public class Compiler {
 
-    public Program compile( char []p_input ) {
-        input = p_input;
-        tokenPos = 0;
+  public Program compile( char []p_input ) {
+    input = p_input;
+    tokenPos = 0;
+    nextToken();
+
+    Program e = program();
+    if (tokenPos != input.length)
+    error();
+
+    return e;
+  }
+
+
+  // Program ::= Decl
+  private Program program() {
+    return new Program(decl());
+  }
+
+  // Decl ::= 'v' 'm' '(' ')' StmtBlock
+  private Decl decl() {
+    Decl decl = null;
+    if (token == 'v') {
+      nextToken();
+      if (token == 'm') {
         nextToken();
+        if (token == '(') {
+          nextToken();
+          if (token = ')') {
+            nextToken();
+            decl = new Decl(stmtBlock());
+          }
+        }
+      }
+    }
+    return decl;
+  }
 
-        Program e = program();
-        if (tokenPos != input.length)
-          error();
+  // StmtBlock ::= '{' { VariableDecl } { Stmt } '}'
+  private StmtBlock stmtBlock() {
+    ArrayList<VariableDecl> retorno = new ArrayList<VariableDecl>();
+    while(token == 'i' || token == 'd' || token == 'c'){
 
-        return e;
+    }
+    ArrayList<Stmt> varDec = variableDecl();
+
+  }
+
+  // VariableDecl ::= Variable ';'
+  private ArrayList<Variable> variableDecl() {
+
+    return varList;
+  }
+
+  // Variable ::= Type Ident
+  private Variable variable() {
+    type();
+    ident();
+  }
+
+  // Type ::= StdType | ArrayType
+  private Type type() {
+    if (token == 'i' || token == 'd' || token == 'c') {
+      nextToken();
+      else error();
     }
 
-
-    // Program ::= Decl
-    private Program program() {
-        return new Program(decl());
-    }
-
-    // Decl ::= 'v' 'm' '(' ')' StmtBlock
-    private Decl decl() {
-
-        return decl;
-    }
-
-    // StmtBlock ::= '{' { VariableDecl } { Stmt } '}'
-    private StmtBlock stmtBlock() {
-        List<Variable> = variableDecl();
-    }
-
-    // VariableDecl ::= Variable ';'
-    private List<Variable> variableDecl() {
-
-
-        return varList;
-    }
-    // Variable ::= Type Ident
-    // Type ::= StdType | ArrayType
     // StdType ::= 'i' | 'd' | 'c'
+    private StdType stdType() {
+      if (token == 'i' || token == 'd' || token == 'c') {
+        nextToken();
+      } else error();
+    }
+
     // ArrayType ::= StdType '[' ']'
+    private ArrayType arrayType() {
+
+      if (token == '[') {
+        nextToken();
+        if (token == ']') {
+          nextToken();
+        } else error();
+      } else error();
+    }
+
     // Stmt ::= Expr ';' | IfStmt | WhileStmt | BreakStmt | PrintStmt
+    private Stmt stmt() {
+
+    }
+
     // IfStmt ::= 'f' '(' Expr ')' '{' { Stmt } '}' [ 'e' '{' { Stmt } '}' ]
+    private IfStmt ifStmt() {
+
+    }
+
     // WhileStmt ::= 'w' '(' Expr ')' '{' { Stmt } '}'
+    private WhileStmt whiteStmt() {
+      WhileStmt whileStmt = new WhileStmt();
+      if (token == 'w') {
+        nextToken();
+        if (token == '(') {
+          nextToken();
+          whileStmt.setExpr(expr());
+          if (token == ')') {
+            nextToken();
+            if (token == '{') {
+              nextToken();
+              whileStmt.setStmt(stmt());
+              if (token == '}') {
+                nextToken();
+              } else error();
+            } else error();
+          } else error();
+        } else error();
+      } else error();
+
+      return whileStmt;
+    }
+
     // BreakStmt ::= 'b' ';'
+    private BreakStmt breakStmt() {
+      if (token == 'b') {
+        nextToken();
+        if (token == ';') {
+          nextToken();
+        } else error();
+      } else error();
+    }
+
     // PrintStmt ::= 'p' '(' Expr { ',' Expr }')'
+    private PrintStmt printStmt() {
+      if (token == 'p') {
+        nextToken();
+        if (token == '(')) {
+          expr();
+          while(token == ',') {
+            nextToken();
+            expr();
+          }
+        } else error();
+      } else error();
+    }
+
     // Expr ::= SimExpr [ RelOp Expr]
+    private Expr expr() {
+
+    }
+
     // SimExpr ::= [Unary] Term { AddOp Term }
+    private SimExpr simExpr() {
+
+    }
+
     // Term ::= Factor { MulOp Factor }
-    // Factor ::= LValue '=' Expr | LValue | '(' Expr ')'
+    private Term term() {
+
+    }
+
+    // Factor ::= LValue ':' Expr | LValue | '(' Expr ')'
     //              | 'r' '(' ')' | 's' '(' ')' | 't' '(' ')'
     private Factor factor() {
-        Factor factor = null;
-        return factor;
+      Factor factor = null;
+      if (token == 'r' && ){
+        nextToken
+      }
+      return factor;
     }
 
     // LValue ::= Ident | Ident '[' Expr ']'
     private LValue lValue() { //# LValue return a string (within LValue class!!!)
 
-        return lValue;
+      return lValue;
     }
 
 
     // Ident ::= Letter { Letter | Digit }
     private Ident ident() {
 
-        return ident;
+      return ident;
     }
 
     // RelOp ::= '=' | '#' | '<' | '>'
     private RelOp relOp() {
-        RelOp relOp = null;
-        if (token == '=' || token == '#' || token == '<' || token == '>'){
-            relOp = new RelOp(token);
-            nextToken();
-        } else error();
-        return relOp;
+      RelOp relOp = null;
+      if (token == '=' || token == '#' || token == '<' || token == '>'){
+        relOp = new RelOp(token);
+        nextToken();
+      } else error();
+      return relOp;
     }
 
     // AddOp ::= '+' | '-'
     private AddOp addOp() {
-        AddOp addOp = null;
-        if (token == '+' || token == '-'){
-            addOp = new AddOp(token);
-            nextToken();
-        } else error();
-        return addOp;
+      AddOp addOp = null;
+      if (token == '+' || token == '-'){
+        addOp = new AddOp(token);
+        nextToken();
+      } else error();
+      return addOp;
     }
 
     // MulOp ::= '*' | '/' | '%'
     private MulOp mulOp() {
-        MulOp mulOp = null;
-        if (token == '*' || token == '/' || token == '%'){
-            mulOp = new MulOp(token);
-            nextToken();
-        } else error();
-        return mulOp;
+      MulOp mulOp = null;
+      if (token == '*' || token == '/' || token == '%'){
+        mulOp = new MulOp(token);
+        nextToken();
+      } else error();
+      return mulOp;
     }
 
     // Unary ::= '+' | '-' | '!'
     private Unary unary() {
-        Unary un = null;
-        if (token == '+' || token == '-' || token == '!'){
-            un = new Unary(token);
-            nextToken();
-        } else error();
-        return un;
+      Unary un = null;
+      if (token == '+' || token == '-' || token == '!'){
+        un = new Unary(token);
+        nextToken();
+      } else error();
+      return un;
     }
 
     // Digit ::= '0' | '1' | ... | '9'
     private Digit digit() {
-        Digit num = null;
-        if (c >= '0' && c <= '9') {
-            num = new Digit(token);
-            nextToken();
-        } else error();
-        return num;
+      Digit num = null;
+      if (c >= '0' && c <= '9') {
+        num = new Digit(token);
+        nextToken();
+      } else error();
+      return num;
     }
 
     // Letter ::= 'A' | 'B' | ... | 'Z' | 'a' | 'b' | ... | 'z'
     private char letter(){
-        char let = ' ';
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-            let = token;
-            nextToken();
-        } else {
-            error();
-        }
-        return let;
+      char let = ' ';
+      if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+        let = token;
+        nextToken();
+      } else {
+        error();
+      }
+      return let;
     }
 
     // END OF GRAMMAR ============================================
     private void nextToken() {
-  		while(tokenPos < input.length && input[tokenPos] == ' ')
-  			tokenPos++;
+      while(tokenPos < input.length && input[tokenPos] == ' ')
+      tokenPos++;
 
-  		if(tokenPos >= input.length)
-  			token = '\0';
-  		else {
-  			token = input[tokenPos];
-  			tokenPos++;
-  		}
+      if(tokenPos >= input.length)
+      token = '\0';
+      else {
+        token = input[tokenPos];
+        tokenPos++;
+      }
     }
 
     private void error() {
-        if ( tokenPos == 0 )
-          tokenPos = 1;
-        else
-          if ( tokenPos >= input.length )
-            tokenPos = input.length;
+      if ( tokenPos == 0 )
+      tokenPos = 1;
+      else
+      if ( tokenPos >= input.length )
+      tokenPos = input.length;
 
-        String strInput = new String( input, tokenPos - 1, input.length - tokenPos + 1 );
-        String strError = "Error at \"" + strInput + "\"";
-        System.out.println( strError );
-        throw new RuntimeException(strError);
+      String strInput = new String( input, tokenPos - 1, input.length - tokenPos + 1 );
+      String strError = "Error at \"" + strInput + "\"";
+      System.out.println( strError );
+      throw new RuntimeException(strError);
     }
 
     private char token;
     private int  tokenPos;
     private char []input;
 
-}
+  }
